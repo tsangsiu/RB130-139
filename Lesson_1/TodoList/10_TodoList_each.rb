dd0af0534ab56af45a1d55dd27a8d5f8ca0f0421
tsangsiu <tsangsiu@gmail.com>
2023-02-20 04:19:46 +0000

    Done TodoList

---
+++ Lesson_1/TodoList/10_TodoList_each.rb
@@ -0,0 +1,111 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(otherTodo)
+    title == otherTodo.title &&
+      description == otherTodo.description &&
+      done == otherTodo.done
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(todo)
+    raise TypeError, "Can only add Todo objects" unless todo.class == Todo
+    todos << todo
+  end
+  alias_method :<<, :add
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def to_a
+    todos.clone
+  end
+
+  def done?
+    todos.all? { |todo| todo.done? == true }
+  end
+
+  def item_at(index)
+    todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def done!
+    todos.each { |todo| todo.done! }
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    todos.delete(item_at(index))
+  end
+
+  def to_s
+    ["---- #{title} ----", todos.map { |todo| todo.to_s }].join("\n")
+  end
+  
+  def each
+    todos.each do |todo|
+      yield(todo)
+    end
+  end
+
+  private
+
+  attr_reader :todos
+end
