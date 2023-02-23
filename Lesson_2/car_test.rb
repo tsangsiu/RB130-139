3f8d3841974ffa4bf5bf1a157dfd7df5946a91cb
tsangsiu <tsangsiu@gmail.com>
2023-02-23 08:12:50 +0000

    Rewrite car_test.rb using the setup method

--- Lesson_2/car_test.rb
+++ Lesson_2/car_test.rb
@@ -5,9 +5,12 @@ Minitest::Reporters.use!
 require_relative 'car'
 
 class CarTest < Minitest::Test
+  def setup
+    @car = Car.new    
+  end
+
   def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
+    assert_equal(4, @car.wheels)
   end
 
   def test_bad_wheels
@@ -17,31 +20,27 @@ class CarTest < Minitest::Test
   end
 
   def test_car_exists
-    car = Car.new
-    assert(car)
+    assert(@car)
   end
 
   def test_name_is_nil
-    car = Car.new
-    assert_nil(car.name)
+    assert_nil(@car.name)
   end
 
   def test_raise_initialize_with_arg
     assert_raises(ArgumentError) do
-      car = Car.new(name: "Joey")
+      Car.new(name: "Joey")
     end
   end
 
   def test_instance_of_car
-    car = Car.new
-    assert_instance_of(Car, car)
+    assert_instance_of(Car, @car)
   end
 
   def test_includes_car
-    car = Car.new
     arr = [1, 2, 3]
     arr << car
 
-    assert_includes(arr, car)
+    assert_includes(arr, @car)
   end
 end
