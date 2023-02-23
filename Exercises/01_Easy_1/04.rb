ac8850bd3e1c0f229739df38d34a1657b1c09372
tsangsiu <tsangsiu@gmail.com>
2023-02-24 00:10:05 +0800

    Done Exercise: Easy 1 #4 FE

--- Exercises/01_Easy_1/04.rb
+++ Exercises/01_Easy_1/04.rb
@@ -8,9 +8,18 @@ end
 
 # Further Exploration
 
+def divisors(int)
+  divisor = []
+  (1..(Math.sqrt(int).to_i)).each do |num|
+    divisor << num << int / num if int % num == 0
+  end
+  divisor.uniq.sort
+end
+
 p divisors(1) == [1]
 p divisors(7) == [1, 7]
 p divisors(12) == [1, 2, 3, 4, 6, 12]
 p divisors(98) == [1, 2, 7, 14, 49, 98]
 p divisors(99400891) == [1, 9967, 9973, 99400891]
 p divisors(999962000357) == [1, 999979, 999983, 999962000357]
+p divisors(9898004545065487)
