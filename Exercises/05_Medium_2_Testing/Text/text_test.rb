34af85ca00fcf1b3ed7331a56ae835e640fd5705
tsangsiu <tsangsiu@gmail.com>
2023-05-07 18:26:05 +0800

    Redo Ex: Medium 2 Testing text

--- Exercises/05_Medium_2_Testing/Text/text_test.rb
+++ Exercises/05_Medium_2_Testing/Text/text_test.rb
@@ -1,14 +1,14 @@
 require 'minitest/autorun'
 require_relative 'text'
 
-class TextTest < Minitest::Test
+class TextTest < MiniTest::Test
   def setup
-    @file = File.open('sample_text.txt', 'r')
+    @file = File.new("sample_text.txt", "r")
+    @text = Text.new(@file.read)
   end
 
   def test_swap
-    text = Text.new(@file.read)    
-    actual_text = text.swap('a', 'e')
+    actual_text = @text.swap('a', 'e')
     expected_text = <<~TEXT.chomp
     Lorem ipsum dolor sit emet, consectetur edipiscing elit. Cres sed vulputete ipsum.
     Suspendisse commodo sem ercu. Donec e nisi elit. Nullem eget nisi commodo, volutpet
@@ -17,17 +17,16 @@ class TextTest < Minitest::Test
     dolor ornere bibendum. Morbi ut messe nec lorem tincidunt elementum vitee id megne. Cres
     et verius meuris, et pheretre mi.
     TEXT
+
     assert_equal(expected_text, actual_text)
   end
 
   def test_word_count
-    text = Text.new(@file.read)    
-    actual_word_count = text.word_count
-    expected_word_count = 72
-    assert_equal(expected_word_count, actual_word_count)
+    assert_equal(72, @text.word_count)
   end
 
   def teardown
     @file.close
+    puts "File has been closed: #{@file.closed?}"
   end
 end
