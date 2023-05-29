35d5316d17c87c9f754391deafdccc2b795cddf3
tsangsiu <tsangsiu@gmail.com>
2023-05-29 14:33:09 +0800

    Re-do easy Ruby Challenge #1

--- Ruby_Challenges/01_Easy/triangles.rb
+++ Ruby_Challenges/01_Easy/triangles.rb
@@ -1,61 +1,76 @@
 =begin
 
-# Problem
-- to write a program to determine if a triangle is equilateral, isosceles, or scalene
+[P] Understanding the Problem
+- Write a program to determine if a triangle is equilateral, isosceles, or scalene,
+  given its three sides
+- Equilateral
+  - three sides are equal
+- Isosceles
+  - two of the sides are equal
+- Scalene
+  - all sides are of different length
+- To form a triangles
+  - length > 0
+  - the sum of any two sides must be greater than the 3rd side
 
-# Example
-Triangle.new(2, 2, 2) == 'equilateral'
-Triangle.new(3, 4, 4) == 'isosceles'
-Triangle.new(3, 4, 5) == 'scalene'
-Triangle.new(0, 0, 0) raises ArgumentError
-Triangle.new(3, 4, -5) raises ArgumentError
-Triangle.new(1, 1, 3) raises ArgumentError
+[E] Examples / Test Cases
+(2, 2, 2) => 'equilateral'
+(3, 4, 4) => 'isosceles'
+(3, 4, 5) => 'scalene'
+(0.3, 0.4, 0.6) => 'scalene'
+(0, 0, 0) => ArgumentError, length 0
+(3, 4, -5) => ArgumentError, -ve length
+(1, 1, 3) => ArgumentError, invalid sides
+(1, 1, 2) => ArgumentError, invalid sides
 
-# Data Structure
-- input: three numbers, representing sides
-- intermediate: array?
-- output: string
+[D] Data Structure
+- Input: Array, containing three numbers
+- Output: String, or raising ArgumentError
 
-# Algorithm
-- constructor
-  - accepts three arguments as three sides when instantiation
-  - raise ArgumentError if the three given sizes are illegal
+[A] Algorithm
+- Triangle class
+  - accepts three numbers upon instantiation
+  - raises ArgumentError if "invalid"
+  
+- help method: valid?
+  - return false if length <= 0 or sum of the any two sides is shorter than the 3rd side
 
-- `Triangle#valid_sides?`
-  - return false if one of the sides <= 0
-  - return false if the sum of any two sides is smaller than or equal to the third side
-  - return true
-
-- `Triangle#kind`
-  - check if three sides are equal, return 'equilateral' if true
-  - check if two of the sides are equal, return 'isosceles' if true
-  - return 'scalene'
-
+- Triangle#kind
+  - If three sides are equal
+    - return 'equilateral'
+  - Else if two sides are equal
+    - return 'isosceles'
+  - Else
+    - return 'scalene'
 =end
 
 class Triangle
   def initialize(side1, side2, side3)
     @sides = [side1, side2, side3]
-    raise ArgumentError unless valid_sides?
+    raise ArgumentError unless valid_triangle?
   end
 
   def kind
-    if @sides.uniq.size == 1
-      return 'equilateral'
-    elsif @sides.uniq.size == 2
-      return 'isosceles'
-    else
-      return 'scalene'
+    case @sides.uniq.size
+    when 1 then 'equilateral'
+    when 2 then 'isosceles'
+    else        'scalene'
     end
   end
 
   private
 
+  def valid_triangle?
+    valid_lengths? && valid_sides?
+  end
+
+  def valid_lengths?
+    @sides.all? { |side| side > 0 }
+  end
+
   def valid_sides?
-    return false if @sides.any? { |side| side <= 0 }
-    return false if @sides[0] + @sides[1] <= @sides[2] ||
-                    @sides[0] + @sides[2] <= @sides[1] || 
-                    @sides[1] + @sides[2] <= @sides[0]
-    true
+    @sides[0] + @sides[1] > @sides[2] &&
+    @sides[0] + @sides[2] > @sides[1] &&
+    @sides[1] + @sides[2] > @sides[0]
   end
 end
