c038b4671608ae915434b35acce0c1a2381e45f8
tsangsiu <tsangsiu@gmail.com>
2023-06-02 18:30:43 +0800

    Re-do easy Ruby Challenge #5

--- Ruby_Challenges/01_Easy/scrabble_score.rb
+++ Ruby_Challenges/01_Easy/scrabble_score.rb
@@ -1,67 +1,75 @@
 =begin
 
-# Problem
-- write a program that, given a word, computes the Scrabble score for that word
+[P]
+- Write a program that calculates a word's scrabble score
+- The score is calculated by summing the scores of individual characters
+- Case-insensitive
+- Non alphabets (blanks, \t, \n, nil) score no point
+- An alphabet-to-score table is given
 
-# Examples
-- 'CABBAGE' = 1*2 + 3*2 + 3 + 1 + 2
-- create a `Scrabble` class, which has a `score` method
-- empty string, non-alphabetical string, `nil` score `0`
-- case-insensitive
-- raise no-error
+[E]
+- 'CABBAGE'
+  -> 3 + 1*2 + 3*2 + 2 + 1 = 14
+- 'Cabbage'
+  -> 3 + 1*2 + 3*2 + 2 + 1 = 14
+- " \t\n"
+  -> 0
+- ""
+  -> 0
 
-# Data Structure
-- input: a string, word
-- output: integer, score
+[D]
+- Input: string
+- Intermediate: Array to store individual characters
+- Output: Integer
 
-# Algorithm
-- create a constant hash for the scores in the class
+[A]
+- We need a `Scrabble` class
+  - which includes the `score` instance method
+  - includes a `score` instance method
+  - includes also a `score` class method
+  - includes a hash for alphabet-to-score conversion
 
-- constructor
-  - accept a string (or anything else) as an argument
-  - raise no error
-  
 - `Scrabble#score`
-  - initialize `score` to `0`
-  - return `score` if the passed-in argument is not a string
-  - for the string argument
-    - remove all whitespace characters
-    - turn to upcase
-    - split into individual characters
-  - iterate through the array of individual characters
-    - add the corresponding score to `score`
+  - Accepts an argument
+  - return 0 if the argument is not a string
+  - Split the word into individual characters and get rid of the non-alphabets
+  - Turn all alphabets to uppercase letters
+  - Initialize a variable called `score` to 0
+  - Iterate through the array of alphabets
+    - Increment `score` by the corresponding alphabet score
   - return `score`
 
 - `Scrabble::score`
-  - similar to that of `Scrabble#score`
+  - Call the instance method `score` and pass in the word as an argument
 
 =end
 
 class Scrabble
-  SCORES = { 'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1,
-              'L' => 1, 'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1,
-              'D' => 2, 'G' =>	2,
-              'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3,
-              'F' => 4, 'H' => 4, 'V' => 4, 'W' => 4, 'Y' => 4,
-              'K' => 5,
-              'J' => 8, 'X' => 8,
-              'Q' => 10, 'Z' => 10 }
-  
-  def initialize(str)
-    @str = str
+  ALPHABET_TO_VALUE = {
+    'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1,
+    'L' => 1, 'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1,
+    'D' => 2, 'G' => 2,
+    'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3,
+    'F' => 4, 'H' => 4, 'V' => 4, 'W' => 4, 'Y' => 4,
+    'K' => 5,
+    'J' => 8, 'X' => 8,
+    'Q' => 10, 'Z' => 10
+  }.freeze
+
+  def initialize(word)
+    @word = word
   end
 
   def score
+    return 0 if @word.class != String
+
     score = 0
-    return score unless @str.class == String
-    chars = @str.gsub(/\s/, "").upcase.chars
-    chars.each do |char|
-      score += SCORES[char]
-    end
+    chars = @word.upcase.chars.select { |char| char =~ /[a-z]/i }
+    chars.each { |char| score += ALPHABET_TO_VALUE[char] }
     score
   end
-  
-  def self.score(str)
-    self.new(str).score
+
+  def self.score(word)
+    new(word).score
   end
 end
