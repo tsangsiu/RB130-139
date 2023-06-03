5e40a13eb558543f9b528fd56f99944ce0ddf164
tsangsiu <tsangsiu@gmail.com>
2023-06-03 23:17:21 +0800

    Re-do easy Ruby Challenge #6

--- Ruby_Challenges/01_Easy/perfect_numbers.rb
+++ Ruby_Challenges/01_Easy/perfect_numbers.rb
@@ -1,74 +1,80 @@
 =begin
 
-# Problem
-- write a program to determine if a number is perfect, abundant or deficient
+[P]
+- Natural numbers can be uniquely categorised to abundant, perfect or deficient
+- This classification is based on the sum of its divisors
+  - Excluding the number itself
+  - The sum is called the Aliquot sum
+- Aliquot sum = original number => perfect numbers
+- Aliquot sum < original number => deficient numbers
+- Aliquot sum > original number => abundant numbers
 
-# Examples
-- 6; divisors 1, 2, 3; 1 + 2 + 3 = 6 => perfect
-- 28; divisors 1, 2, 4, 7, 14; 1 + 2 + 4 + 7 + 14 = 28 => perfect
-- 15; divisors 1, 3, 5; 1 + 3 + 5 = 9 < 15 => deficient
-- 24; divisors 1, 2, 3, 4, 6, 8, 12; 1 + 2 + 3 + 4 + 6 + 8 + 12 = 36 > 24 => adundant
-- prime numbers are always deficient
-- the Aliquot sum of a number is the sum of its divisors excluding itself
+[E]
+- 13
+  - divisors: 1
+  - Aliquot sum = 1
+  - Aliquot sum < 13 => deficient numbers
+- 28
+  - divisors: 1, 2, 4, 7, 14
+  - Aliquot sum = 1 + 2 + 4 + 7 + 14 = 28
+  - perfect number
+- 12
+  - divisors: 1, 2, 3, 4, 6
+  - Aliquot sum = 1 + 2 + 3 + 4 + 6 = 16 > 12
+  - abundant number
 
-- we need a `PerfectNumber` class
-- and a class method `::classify`
-  - accepts an integer, returns a string (classification)
-  - raises StandardError if not a positive integer
+[D]
+- Input: Integer
+- Output: String
 
-# Data Structure
-- input: integer
-- output: string, 'deficient', 'perfect', or 'abundant'
+[A]
+- We need a `PerfectNumber` class
+  - has a class method called `classify` and takes an integer
 
-# Algorithm
-- helper method: `PerfectNumber::factors`
-  - input: a positive integer
-  - output: an array of its factors, excluding itself
-  - raise StandardError if the argument is not a positive integer
-  - initialize `factors` to an empty array
-  - iterate from 1 to square root of the given number, say `n`
-    - if the current number is a factor,
-      - push the current number to `factors`
-      - push also `n`/current number to `factots`
-  - remove `n` from `factors`
-  - return the sorted `factors`
-  
 - `PerfectNumber::classify`
-  - takes a positive integer as an argument
-  - raise StandardError if not
-  - get an array of factors of the argument, exclusing the argument itself
-  - sum all the factors
-  - compare the sum and the argument
-  - if sum < argument
-    - return 'deficient'
-  - else if sum == argument
-    - return 'perfect'
-  - else if sum > argument
+  - accepts an integer
+  - raise `StandardError` if the number is not a natural number
+  - get all divisors of the number except itself
+  - sum all the divisors
+  - if the sum > the number
     - return 'abundant'
+  - else if the sum < the number
+    - return 'deficient'
+  - else
+    - return 'perfect'
+
+- Helper method: to get all divisors
+  - accepts an integer
+  - initialize an empty array to contain all divisors
+  - iterate from 1 to square root of the number
+    - if the current number is a divisor
+      - push the number to the empty array
+  - return the array
 
 =end
 
 class PerfectNumber
-  def self.classify(int)
-    raise StandardError if int.class != Integer || int <= 0
-    factors = factors(int)
-    sum_of_factors = factors.sum
-    if sum_of_factors < int
-      return 'deficient'
-    elsif sum_of_factors == int
-      return 'perfect'
+  def self.classify(num)
+    raise StandardError unless num > 0 && num.class == Integer
+    aliquot_sum = divisors(num).sum
+    if aliquot_sum < num
+      'deficient'
+    elsif aliquot_sum > num
+      'abundant'
     else
-      return 'abundant'
+      'perfect'
     end
   end
-  
-  def self.factors(int)
-    raise StandardError if int.class != Integer || int <= 0
-    factors = []
-    1.upto(Math::sqrt(int)) do |num|
-      factors << num << int / num if int % num == 0
+
+  class << self
+    private
+
+    def divisors(num)
+      divisors = []
+      (1..Math.sqrt(num)).each do |divisor|
+        divisors << divisor << num / divisor if num % divisor == 0
+      end
+      divisors.sort[0...-1].uniq
     end
-    factors.delete(int)
-    factors.sort!
   end
 end
