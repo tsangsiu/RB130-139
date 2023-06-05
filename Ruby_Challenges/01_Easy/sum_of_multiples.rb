1ec8c10011f7cd6ed821c0a688ea5076ae7bb6c2
tsangsiu <tsangsiu@gmail.com>
2023-06-05 20:26:54 +0800

    Re-do easy Ruby Challenge #8

--- Ruby_Challenges/01_Easy/sum_of_multiples.rb
+++ Ruby_Challenges/01_Easy/sum_of_multiples.rb
@@ -1,93 +1,83 @@
 =begin
 
-# Problem
-- given a natual number N
-- given another set of numbers M1, M2, ...
-- M1, M2, ... may not be given
-- find the sum of each of the multiples of M1, M2, ... that are smaller than N
-- if M1, M2, ... are not given, use M1 = 3 and M2 = 5
+[P]
+- Given a natural number (1, 2, 3, ...) and a set of one or more other numbers
+- Find the sum of the multiples of the numbers in the set
+  that are less than the natural number
+- If the set of number is not given, use a default set of 3 and 5
 
-# Examples
-- N = 10, F1, F2, ... not given
-  N = 10, F1 = 3, F2 = 5
-  multiples = [3, 5, 6, 9]
-  sum = 3 + 5 + 6 + 9 = 23
-- N = 20, F1 = 7, F2 = 13, F3 = 17
-  multiples = [7, 14, 13, 17]
-  sum = 7 + 14 + 13 + 17 = 51
+[E]
+- From the examples given, we need a `SumOfMultiples` class
+  - It accepts one or more integers as arguemnts upon instantiation
+  - It has a `to` class method and a `to` instance method
+  - It seems that it is assumed that arguments passed in are valid
+- Given a natural number 10, a set of number not given
+  - Multiples of 3 and 5 that are up to, but not including 10: 3, 5, 6, 9
+  - Sum = 3 + 5 + 6 + 9 = 23
+- Given number 20, a set of number [7, 13, 17]
+  - Multiples of 7, 13 and 17 that are up to, but not including 20:
+    7, 13, 14, 17
+  - Sum = 7 + 13 + 14 + 17 = 51
 
-- from the test cases, we need to create a `SumOfMultiples` class
-- the `SumOfMultiples` class has the following three methods:
-  - the constructor
-    - it accepts integers representing M1, M2, ...
-  - `SumOfMultiples#to`
-    - it accepts an integer representing N
-  - `SumOfMultiples::to`
-    - it accepts an integer representing N
-    - M1 = 3 and M2 = 5
+[D]
+- Input: Integer (natural number), Array (a set of numbers)
+- Output: Integer
 
-# Data Structure
-- input: integers representing N and/or M1, M2, ...
-- intermediate: array to store the multiples
-- output: integer, the sum of all the desired multiples
-
-# Algorithm
-- constructor
-  - accepts an unknown number of integers representing M1, M2, ...
-  - assign the integers to an array to an instance variable `@m_s`
+[A]
+- The `SumOfMultiples` class
+  - Accepts multiple integers upon instantiation
+  - Raises no error
 
 - `SumOfMultiples#to`
-  - takes an integer representing N as an argument
-  - output the sum of the desired multiples
-  - initialize `multiples` to an empty array
-  - for each of the number in `@m_s`,
-    - get the their multiples up to, but not including N
-    - push the multiples to `multiples`
-  - remove duplicate numbers in `multiples`
-  - return the sum of the numbers in `multiples`
+  - Accepts an integer (the natural number) as an argument
+  - Find the multiples of each number in the set that are up to,
+    but not including, the natural number
+  - Sum the unique multiples
+  - Return the sum
 
-- helper method: `SumOfMultiples#multiples`
-  - takes two integers, N and M as arguments
-  - initialize `multiples` to an empty array
-  - initialize `num` to M
-  - while `num` < N
-    - push `num` to `multiples`
-    - increment `num` by M
-  - return `multiples`
+- `SumofMultiples::to`
+  - Accepts an integer (the natural number) as an argument
+  - Find the multiples of 3 and 5 that are up to,
+    but not including, the natural number
+  - Sum the unique multiples
+  - Return the sum
 
-- `SumOfMultiples::to`
-  - takes an integer representing N as an argument
-  - output the sum of the desided multiples
-  - instantiate a `SumOfMultiples` object with M1 = 3 and M2 = 5
-  - call `SumOfMultiples#to` on the above object with the passed-in argument
+- Helper method: find the multiples
+  - Given a natural number and a number `n`
+  - Initialize an array with a single element 0 to store the multiples
+  - Initialize a variable called `multiplier` to 1
+  - Until `n` * `multiplier` >= natural number
+    - Push `n` * `multiplier` to the array
+    - Increment `multiplier` by 1
+  - Return the array
 
 =end
 
 class SumOfMultiples
-  def initialize(*m)
-    @m_s = m
+  def initialize(*int)
+    @n = int
   end
 
-  def self.to(n)
-    self.new(3, 5).to(n)
-  end
-
-  def to(n)
-    multiples = []
-    @m_s.each do |m|
-      multiples << multiples(n, m)
+  def to(natural_n)
+    multiples = [0]
+    @n.each do |n|
+      multiples += multiples(n, natural_n)
     end
-    multiples.flatten.uniq.sum
+    multiples.uniq.sum
+  end
+
+  def self.to(natural_n)
+    new(3, 5).to(natural_n)
   end
 
   private
 
-  def multiples(n, m)
+  def multiples(n, natural_n)
     multiples = []
-    num = m
-    while num < n
-      multiples << num
-      num += m
+    multiplier = 1
+    until n * multiplier >= natural_n
+      multiples << n * multiplier
+      multiplier += 1
     end
     multiples
   end
