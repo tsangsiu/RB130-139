48b8fa16d4ff65872df6a84cff2c07387a2bfe8b
tsangsiu <tsangsiu@gmail.com>
2023-06-27 08:12:26 +0000

    Solved Ruby Challenge Medium #3 with another approach

--- Ruby_Challenges/02_Medium/clock.rb
+++ Ruby_Challenges/02_Medium/clock.rb
@@ -59,7 +59,7 @@
 
 =end
 
-class Clock
+class OriginalClock
   attr_reader :hour, :minute
 
   HOURS_PER_DAY = 24
@@ -98,5 +98,93 @@ class Clock
 
   def -(minute_to_subtract)
     self.+(-minute_to_subtract)
+  end
+end
+
+=begin
+
+Alternative Approach: Without considering modulus
+
+[A]
+- `Clock#+`
+  - Compute the number of minute from midnight
+  - Add the argument passed in to the above number
+  - Convert the result (the number of minute from midnight) back to hour
+    and minute
+  - Return a new `Clock` object with the new hour and minute
+
+- `Clock#-`
+  - Make use of the `Clock#+` method
+
+- Helper Method: `Clock#time_to_minute_since_midnight`
+  - Accepts two arguements: the hour and the minute comprising the time
+  - Return hour * 60 + minute
+
+- Helper Method: `Clock#minute_since_midnight_to_time`
+  - Accepts an integer, the number of minute from midnight, as an argument
+  - It can be any integers
+  - If the integer is not between 0 and 1439 inclusively,
+    - keep adding 1440 if the integer < 0 until 0 <= integer <= 1439
+    - keep subtracting 1440 if the integer > 1439 until 0 <= integer <= 1439
+  - Divide the integer by 60,
+    - the quotient is the hour
+    - the remainder is the minute
+  - Return them in an array: [hour, minute]
+
+=end
+
+class Clock
+  attr_reader :hour, :minute
+
+  HOURS_PER_DAY = 24
+  MINUTES_PER_HOUR = 60
+  MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY
+
+  def initialize(hour, minute)
+    @hour = hour
+    @minute = minute
+  end
+
+  def self.at(hour, minute = 0)
+    new(hour, minute)
+  end
+
+  def to_s
+    hour_str = hour > 9 ? hour.to_s : "0#{hour}"
+    minute_str = minute > 9 ? minute.to_s : "0#{minute}"
+    "#{hour_str}:#{minute_str}"
+  end
+
+  def ==(clock)
+    hour == clock.hour && minute == clock.minute
+  end
+
+  def +(minute_to_add)
+    minute_since_midnight = time_to_minute_since_midnight(@hour, @minute)
+    minute_since_midnight += minute_to_add
+    hour, minute = minute_since_midnight_to_time(minute_since_midnight)
+    self.class.new(hour, minute)
+  end
+
+  def -(minute_to_subtract)
+    self.+(-minute_to_subtract)
+  end
+
+  private
+
+  def time_to_minute_since_midnight(hour, minute)
+    hour * 60 + minute
+  end
+
+  def minute_since_midnight_to_time(minute_since_midnight)
+    until minute_since_midnight >= 0
+      minute_since_midnight += MINUTES_PER_DAY
+    end
+
+    until minute_since_midnight < MINUTES_PER_DAY
+      minute_since_midnight -= MINUTES_PER_DAY
+    end
+
+    minute_since_midnight.divmod(MINUTES_PER_HOUR)
   end
 end
